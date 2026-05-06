13a232e63de29484b1bb4bfaeec9adc4d2f9d444
miafuentes30 <fue23775@uvg.edu.gt>
2026-05-05 22:15:19 -0600

    feat: add math logic

--- ej1.py
+++ ej1.py
@@ -18,12 +18,29 @@ def convertir_a_centavos(monto):
     return quetzales * 100 + centavos
 
 
+def hacer_sencillo(monto_centavos):
+    monedas = [25, 10, 5, 1]
+    resultado = []
+
+    restante = monto_centavos
+
+    for moneda in monedas:
+        cantidad = restante // moneda
+        resultado.append((moneda, cantidad))
+        restante = restante % moneda
+
+    return resultado
+
 
 def main():
     monto = input("Ingrese el monto en quetzales. Ejemplo 2.93: ")
 
     monto_centavos = convertir_a_centavos(monto)
-    print(monto_centavos)
+    #print(monto_centavos)
+    
+    resultado = hacer_sencillo(monto_centavos)
+    print(resultado)
+    
 
 
 if __name__ == "__main__":
